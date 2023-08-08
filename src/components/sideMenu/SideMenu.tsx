f39482cc84fe2015b2b2a7e486a61e258c487b3b
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-08 18:40:38 +1200

    create a SideMenu component

---
+++ src/components/sideMenu/SideMenu.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Styles from './SideMenu.module.css';
+import { sideMenuList, productList1 } from "./mockup";
+import { Menu } from "antd";
+import { GifOutlined, GiftOutlined } from "@ant-design/icons";
+export const SideMenu: React.FC = () => {
+    return (
+        <Menu
+            className={Styles["side-menu"]}
+            mode={"vertical"}
+            items={sideMenuList.map((m) =>
+            ({
+                label: m.title,
+                key: m.title,
+                icon: <GiftOutlined />,
+                children: m.subMenu.map((sm) =>
+                ({
+                    label: sm.title,
+                    key: sm.title,
+                    icon: <GiftOutlined />,
+                    children: sm.subMenu.map((sm1) => ({ label: sm1, key: sm1, icon: <GiftOutlined /> }))
+                }))
+            }))} >
+        </Menu>
+    );
+}
---
+++ src/components/sideMenu/mockup.ts
@@ -0,0 +1,88 @@
+export const sideMenuList = [
+  {
+    title: "Adventure Travel",
+    subMenu: [
+      {
+        title: "Hiking & Trekking",
+        subMenu: ["Mountain Hiking", "Jungle Trekking", "Desert Trekking"],
+      },
+      {
+        title: "Water Sports & Activities",
+        subMenu: ["Scuba Diving", "Surfing", "White Water Rafting"],
+      },
+      {
+        title: "Wildlife Safaris",
+        subMenu: ["African Safari", "Jungle Safari", "Marine Wildlife Watching"],
+      },
+    ],
+  },
+  {
+    title: "Cultural & Heritage Tourism",
+    subMenu: [
+      {
+        title: "Historical Landmarks",
+        subMenu: ["Ancient Ruins", "UNESCO World Heritage Sites", "Architectural Marvels"],
+      },
+      {
+        title: "Local Festivals & Events",
+        subMenu: ["Religious Festivals", "Cultural Celebrations", "Traditional Folk Festivals"],
+      },
+      {
+        title: "Museums & Art Galleries",
+        subMenu: ["Art Museums", "History Museums", "Ethnographic Museums"],
+      }
+    ],
+  },
+  {
+    title: "Relaxation & Wellness Retreats",
+    subMenu: [
+      {
+        title: "Spa & Wellness Resorts",
+        subMenu: ["Ayurvedic Retreats", "Yoga & Meditation Centers", "Hot Spring Resorts"],
+      },
+      {
+        title: "Beach Getaways",
+        subMenu: ["Tropical Islands", "Mediterranean Coastlines", "Secluded Beaches"],
+      },
+      {
+        title: "Health-focused Holidays",
+        subMenu: ["Detox Retreats", "Weight Loss Retreats", "Wellness Workshops"],
+      },
+    ],
+  },
+  {
+    title: "Culinary Tourism",
+    subMenu: [
+      {
+        title: "Food Tours",
+        subMenu: ["Street Food Tours", "Gastronomic City Tours", "Vineyard & Wine Tasting Tours"],
+      },
+      {
+        title: "Cooking Classes & Workshops",
+        subMenu: ["Local Cuisine Classes", "Baking & Pastry Workshops", "Ethnic Cooking Workshops"],
+      },
+      {
+        title: "Food Festivals & Events",
+        subMenu: ["International Food Festivals", "Culinary Competitions", "Regional Food Celebrations"],
+      },
+    ],
+  }, {
+    title: "Ecotourism",
+    subMenu: [
+      {
+        title: "Nature Reserves & National Parks",
+        subMenu: ["Rainforest Reserves", "Wildlife Sanctuaries", "Coastal Reserves"],
+      },
+      {
+        title: "Sustainable Farm Stays",
+        subMenu: ["Organic Farms", "Agro-tourism", "Permaculture Farms"],
+      },
+      {
+        title: "Eco-Lodges & Green Retreats",
+        subMenu: ["Off-grid Accommodations", "Renewable Energy Retreats", "Treehouses & Eco-friendly Huts"],
+      },
+    ],
+  }
+
+];
+
