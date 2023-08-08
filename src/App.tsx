3cdaa4ee0caebce8fc00fb5c65694f867fa29150
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-08 21:01:04 +1200

    add ProductCollection component on the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer, SideMenu, Carousel } from './components';
-import { Row, Col } from 'antd';
-
+import { Header, Footer, SideMenu, Carousel, ProductCollection } from './components';
+import { Row, Col, Typography } from 'antd';
+import { popularDestinationsProudctList } from './mockups';
+import SideImage1 from './assets/images/sider01.jpg';
+import SideImage2 from './assets/images/sider02.jpg';
+import SideImage3 from './assets/images/sider03.jpg';
 function App() {
   return (
     <div className={styles.App}>
@@ -17,8 +20,12 @@ function App() {
           <Col span={18}>
             <Carousel />
           </Col>
-
         </Row>
+
+        <ProductCollection
+          title={<Typography.Title level={3} type="warning" >Popular Destinations</Typography.Title>}
+          sideImage={SideImage1}
+          products={popularDestinationsProudctList} />
       </div>
       <Footer />
 
