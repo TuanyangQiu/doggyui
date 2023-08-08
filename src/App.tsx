3887f3db6ae128fbde8fc102d5ae34ee512f713e
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-08 17:02:44 +1200

    design the body of the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
 import { Header, Footer } from './components';
-
+import { Row, Col } from 'antd';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles['page-content']} >
+        <Row style={{ marginTop: 20 }} >
+          <Col span={6}>
+            <div style={{ background: "red" }} > Multiple Menus </div>
+          </Col>
+          <Col span={18}>
+            <div style={{ background: "blue" }}>running menus</div>
+          </Col>
+
+        </Row>
+      </div>
       <Footer />
 
     </div>
