cee943a712b592bae73946376c4c23e03fce07b2
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-08 21:00:20 +1200

    create ProductCollection component. ProductionCollection shows some recommened products on the main page

---
+++ src/components/productCollection/ProductImage.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Image, Typography } from 'antd';
+
+interface PropsType {
+
+    id: number | string;
+    size: "large" | "small";
+    title: string;
+    imageSrc: string;
+    price: number | string;
+
+}
+
+export const ProductImage: React.FC<PropsType> = ({ id, size, title, imageSrc, price }) => {
+
+
+    return (<>
+        {
+            (size === "large") ?
+                (<Image src={imageSrc} height={285} width={480}></Image>) :
+                (<Image src={imageSrc} height={120} width={240}></Image>)
+        }
+        <div>
+            <Typography.Text type="secondary">{(size === "large") ? (title.slice(0, 50)) : (title.slice(0, 22))}</Typography.Text>
+            <Typography.Text type="danger">&nbsp;&nbsp;&nbsp;From ${price}</Typography.Text>
+        </div>
+    </>);
+}
+
+
