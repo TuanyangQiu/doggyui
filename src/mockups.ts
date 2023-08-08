6f46bb82a7b8df0fca003d24528a0e87e47b5388
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-08 21:01:30 +1200

    add mock data for test

---
+++ src/mockups.ts
@@ -0,0 +1,93 @@
+export const popularDestinationsProudctList = [
+  {
+    id: 1,
+    title: "Enchanting Serenity - Exploring the Hidden Gems of Bali",
+    price: "11990",
+    touristRoutePictures: [
+      {
+        url: "https://media.istockphoto.com/id/1398780122/photo/panorama-of-cefalu-harbor-at-sunset.jpg?s=612x612&w=0&k=20&c=YcmOD3S_AuPEh2e5T8dW69jj9CV7VIp54Hq8-_XIat4="
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Unveiling the Wonders of Machu Picchu - A Journey to the Past",
+    price: "13290",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Beyond the Pyramids - Egypt's Timeless Treasures and Tombs",
+    price: "4000",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2015/03/16/10/59/sunset-675847_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Captivating Colors of Santorini - A Mediterranean Dream",
+    price: "7399",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2013/07/18/10/56/railroad-163518_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Wilderness Retreat - Discovering Alaska's Untamed Beauty",
+    price: "3499",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2013/11/28/10/36/road-220058_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Tokyo Tales - Embracing the Modern and Traditional in Japan's Capital",
+    price: "5999",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Chasing the Northern Lights - Norway's Arctic Wonder",
+    price: "6199",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Tropical Paradise Found - Adventures in the Maldives",
+    price: "13699",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_1280.jpg",
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "Cultural Marvels of Marrakech - A Sensory Delight",
+    price: "5021",
+    touristRoutePictures: [
+      {
+        url: "https://cdn.pixabay.com/photo/2017/01/19/23/46/church-1993645_640.jpg",
+      },
+    ],
+  },
+];
+
