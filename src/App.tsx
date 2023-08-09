9acd3ebec37a58c36d55cb62f2b81d6ddf5553d0
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-09 22:04:30 +1200

    configure page navigation route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages';
+import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages';
+
 function App() {
   return (
     <div className={styles.App}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} ></Route>
+          <Route path="/signin" element={<SignInPage />} ></Route>
+          <Route path="/register" element={<RegisterPage />} ></Route>
+          <Route path="/detail/:touristRouteId" element={<DetailPage />} > </Route>
+          <Route path="*"  element={<h2>404 Page Not Found: Oops, it seems the page has gone on a coffee break </h2>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
