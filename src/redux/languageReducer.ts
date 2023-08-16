91f15c7d97ac04087f0c6f1b3749ee537480507e
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-16 21:21:09 +1200

    response to language switch for i18n

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,3 +1,6 @@
+import i18n from "i18next";
+
+
 
 export interface LanguageState {
 
@@ -17,6 +20,9 @@ export default (state = defaultLangState, action) => {
 
     switch (action.type) {
         case "change_language":
+            //Change language here, but it is not that good, 
+            //because it violates the requirement of pure function 
+            i18n.changeLanguage(action.payload);
             return { ...state, language: action.payload };//Immutable
         default:
             return state;
