451876d295f96b1afd0f8a17e42b533c0fb1d341
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-19 22:53:54 +1200

    use react-redux in FC header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,27 +4,36 @@ import logo from '../../assets/logo.svg';
 import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd';
 import { GlobalOutlined } from '@ant-design/icons';
 import { useParams, useLocation, useNavigate } from "react-router-dom";
-
+import { useSelector } from "../../redux/hooks";
+import { useDispatch } from "react-redux";
+import { changeLanguageActionCreator } from '../../redux/language/languageActions';
+import { useTranslation } from "react-i18next";
 export const Header: React.FC = () => {
     const params = useParams();
     const location = useLocation();
     const navigate = useNavigate();
+    const language = useSelector(state => state.language);
+    const languageList = useSelector(state => state.languageList);
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
 
-
-
+    const langMenuSwitchHandler = (e) => {
+        console.log('langMenuSwitchHandler', e);
+        dispatch(changeLanguageActionCreator(e.key));
+    }
     return <div className={styles['app-header']}>
         <div className={styles['top-header']}>
             {/* <div  className={styles.inner}> */}
             <Typography.Text>Welcome</Typography.Text>
             <Dropdown.Button
                 style={{ marginLeft: 15 }}
-                overlay={<Menu items={[{ key: "1", label: "中文" },
-                { key: "2", label: "English" }]} />}
-                icon={<GlobalOutlined />}>EN/中  </Dropdown.Button>
+                overlay={<Menu onClick={langMenuSwitchHandler}
+                    items={languageList.map(m => { return { key: m.code, label: m.name } })} />}
+                icon={<GlobalOutlined />}>{language === "en" ? "ENG" : "中文"} </Dropdown.Button>
 
             <Button.Group className={styles['button-group']}>
-                <Button onClick={() => navigate("/register")} >Register</Button>
-                <Button onClick={() => navigate("/signin")} >Sign In</Button>
+                <Button onClick={() => navigate("/register")} >{t("header.register")}</Button>
+                <Button onClick={() => navigate("/signin")} >{t("header.signin")}</Button>
             </Button.Group>
             {/* </div> */}
         </div>
@@ -39,17 +48,22 @@ export const Header: React.FC = () => {
 
 
         </Layout.Header>
-
         <Menu mode={"horizontal"}
             items={[
-                { key: "1", label: "MainPage" },
-                { key: "2", label: "Fligts" },
-                { key: "3", label: "Trains" },
-                { key: "4", label: "Car Rentals" },
-                { key: "5", label: "Attraction&Tour" },
-                { key: "6", label: "Flight+Hotel" },
-                { key: "7", label: "Destination" },
-                { key: "8", label: "Rewards" }]}
+                { key: "1", label: t("header.home_page") },
+                { key: "2", label: t("header.group") },
+                { key: "3", label: t("header.backpack") },
+                { key: "4", label: t("header.private") },
+                { key: "5", label: t("header.cruise") },
+                { key: "6", label: t("header.hotel") },
+                { key: "7", label: t("header.local") },
+                { key: "8", label: t("header.theme") },
+                { key: "9", label: t("header.custom") },
+                { key: "10", label: t("header.visa") },
+                { key: "11", label: t("header.enterprise") },
+                { key: "12", label: t("header.high_end") },
+                { key: "13", label: t("header.outdoor") },
+                { key: "14", label: t("header.insurance") }]}
             className={styles['main-menu']}></Menu>
     </div>;
 }
