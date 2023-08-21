91c091648e41afca44a7524f7424e067c0982c0c
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-21 16:29:58 +1200

    fetch data from DoggyRestApi project instead of mockup data file

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Row, Col, Typography } from 'antd';
+import { Row, Col, Typography, Spin } from 'antd';
 import { Header, Footer, SideMenu, Carousel, ProductCollection, BusinessPartner } from '../../components';
 import Styles from './HomePage.module.css';
-import { popularDestinationsProudctList } from './mockups';
-import ss from '../../assets/images/sider01.jpg';
 import SideImage1 from '../../assets/images/sider01.jpg';
 import SideImage2 from '../../assets/images/sider02.jpg';
 import SideImage3 from '../../assets/images/sider03.jpg';
@@ -12,9 +10,68 @@ import BussPartner2 from '../../assets/images/BussPartner_Instagram.png';
 import BussPartner3 from '../../assets/images/BussPartner_Microsoft.png';
 import BussPartner4 from '../../assets/images/BussPartner_Youtube.png';
 import { withTranslation, WithTranslation } from "react-i18next";
-class HomePageComponent extends React.Component<WithTranslation> {
+import axios from "axios";
+
+interface State {
+    productionList: any[],
+    loading: boolean,
+    requestTouristRoutesError: string | null
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            productionList: [],
+            loading: true,
+            requestTouristRoutesError: null
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const resp = await axios.get("http://127.0.0.1:5022/TouristRoutes?pageNumber=1&pageSize=9",
+                { headers: { Accept: "application/json" } });
+
+            const tempProd = (resp.data as any[]).map((m, index) => {
+                return {
+                    id: index,
+                    title: m.Title,
+                    touristRoutePictures: [{ url: "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg" }],
+                    price: m.Price
+                }
+            });
+
+            this.setState({ productionList: tempProd, loading: false, requestTouristRoutesError: null });
+        } catch (error) {
+            if (error instanceof Error)
+                this.setState({ productionList: [], loading: false, requestTouristRoutesError: error.message });
+        }
+    }
+
+
     render(): React.ReactNode {
         const { t } = this.props;
+        const { productionList, loading, requestTouristRoutesError } = this.state;
+        if (loading) {
+            return (
+                <Spin
+                    size="large"
+                    style={{
+                        marginTop: 200,
+                        marginBottom: 200,
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        width: "100%",
+                    }}
+                />
+            );
+        }
+        if (requestTouristRoutesError) {
+            return <div>Sorry, {requestTouristRoutesError}</div>;
+        }
+
         return (
             <>
                 <Header />
@@ -33,7 +90,8 @@ class HomePageComponent extends React.Component<WithTranslation> {
                     <ProductCollection
                         title={<Typography.Title level={3} type="warning" >{t("home_page.most_popular_dest")}</Typography.Title>}
                         sideImage={SideImage1}
-                        products={popularDestinationsProudctList} />
+                        products={productionList} />
+
                 </div>
                 <BusinessPartner title={t("home_page.business_partner")} imageUrlList={[BussPartner1, BussPartner3, BussPartner2, BussPartner3, BussPartner4, BussPartner1, BussPartner4, BussPartner2, BussPartner3, BussPartner4, BussPartner1]} />
                 <Footer />
