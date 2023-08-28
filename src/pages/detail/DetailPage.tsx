48d1002fb26511bac48e4fb5679c9b841b8c8595
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-28 21:03:36 +1200

    design the overall styles for the DetailPage

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,12 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-
+import axios from "axios";
+import { Spin, Row, Col, DatePicker, Space } from "antd";
+import { Header, Footer } from "../../components";
+import Styles from './DetailPage.module.css';
 type MatchParams = {
     touristRouteId: string;
 }
+const { RangePicker } = DatePicker;
 
 export const DetailPage: React.FC = () => {
 
-    const params = useParams<MatchParams>();
-    return (<h2>Tourist Route id: {params.touristRouteId} </h2>)
+    const { touristRouteId } = useParams<MatchParams>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [productInfo, setProductInfo] = useState<any>(null);
+    const [requestError, setRequestError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchProductDetails = async () => {
+            setLoading(true);
+            try {
+                const { data } = await axios.get(`http://localhost:5022/TouristRoutes/${touristRouteId}`);
+
+                setProductInfo(data);
+            } catch (error) {
+                setRequestError(error instanceof Error ? error.message : "error!");
+            }
+            setLoading(false);
+
+        }
+
+        fetchProductDetails();
+
+    }, []);
+
+
+    if (loading) {
+        return (
+            <Spin
+                size="large"
+                style={{
+                    marginTop: 200,
+                    marginBottom: 200,
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "100%",
+                }}
+            />
+        );
+    }
+
+    if (requestError) {
+        return <div>Sorry, {requestError}</div>;
+    }
+
+    return (
+        <>
+            <Header />
+            <div className={Styles["page-content"]}>
+
+                {/* Product introduction and date selection */}
+                <div className={Styles["product-intro-container"]}></div>
+                <Row>
+                    <Col span={13}></Col>
+                    <Col span={11}>
+                        <RangePicker style={{ marginTop: 20 }} />
+                    </Col>
+                </Row>
+                {/* Anchor Menu */}
+                <div className={Styles["product-detail-anchor"]}></div>
+
+                {/* Product features */}
+                <div className={Styles["product-detail-container"]} id="feature"></div>
+
+                {/* Fees instrument */}
+                <div className={Styles["product-detail-container"]} id="fees"></div>
+
+                {/* notes about the product */}
+                <div className={Styles["product-detail-container"]} id="notes"></div>
+
+                {/* comments */}
+                <div className={Styles["product-detail-container"]} id="comments"></div>
+            </div>
+            <Footer />
+        </>
+    )
 }
