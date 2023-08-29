b7042437158dab068b873b082683bf9535e5ab58
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-29 19:12:18 +1200

    switch to combineReducers that comes from @reduxjs/toolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,16 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import languageReducer from "./language/languageReducer";
 import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
 import thunk from "redux-thunk";
 import { actionLog } from "./middlewares/actionLog";
+import { productDetailSlice } from "./productDetail/slice";
+import { combineReducers } from '@reduxjs/toolkit';
+
+//after used redux-toolkit, the combineReducer should come from redux toolkit instead of redux
 const rootReducer = combineReducers({
     language: languageReducer,
-    recommendProducts: recommendProductsReducer
+    recommendProducts: recommendProductsReducer,
+    productDetailReducer: productDetailSlice.reducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
