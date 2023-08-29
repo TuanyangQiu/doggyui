c0ac11f26395dc606bd4c6b7c6b1f778a9a302aa
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-29 21:00:18 +1200

    create a hook useAppDispatch

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,11 @@
-import { useSelector as useReduxSelector, TypedUseSelectorHook } from "react-redux";
+import { useSelector as useReduxSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
 
-import { RootState } from "./store";
+import { RootState, AppDispatch } from "./store";
 
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
+
+//using react-redux dispatch to dispatch the action created by createAsyncThunk will get the below error
+//Argument of type 'AsyncThunkAction<void, string, AsyncThunkConfig>' is not assignable to parameter of type 'AnyAction'.
+//so, the solution is to create a new dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>();
