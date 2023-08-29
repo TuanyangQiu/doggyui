20b5a02a1ecb0eeea65baacad0793504fc8878f7
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-29 15:27:57 +1200

    create  mock data for comments and pictures

---
+++ src/pages/detail/mockupDetails.ts
@@ -0,0 +1,39 @@
+
+// these mock data will be sent from the backend application DoggyRestApi in the future
+export const mockupComments = [
+    {
+        author: "Emily",
+        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        content: "Embark on a journey through breathtaking landscapes and charming villages on our enchanting countryside escape. Get ready to unwind, explore, and create unforgettable memories!",
+        dateTime: "2020-7-10"
+    },
+    {
+        author: "Michael Smith",
+        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        content: "Discover the heart of adventure along our adrenaline-pumping route! From thrilling outdoor activities to hidden gems, this route is a must for every thrill-seeker and nature enthusiast.",
+        dateTime: "2022-6-21"
+    },
+    {
+        author: "Williams",
+        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        content: "Indulge in a cultural odyssey like no other as you traverse historical landmarks, savor local cuisines, and immerse yourself in the vibrant traditions of each stop along our captivating heritage trail",
+        dateTime: "2023-3-6"
+    }, {
+        author: "Christopher Brown",
+        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        content: "Unveil the beauty of urban gems and architectural wonders on our cosmopolitan city spree. From towering skyscrapers to quaint cafes, experience the perfect blend of modernity and culture.",
+        dateTime: "2023-4-10"
+    }, {
+        author: "Davis",
+        avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        content: "Calling all foodies and culinary aficionados! Embark on a mouthwatering expedition through our gourmet route, where you'll savor diverse flavors, artisanal treats, and the finest dining experiences",
+        dateTime: "2023-7-28"
+    }
+]
+
+export const productPictures = [
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg"]
