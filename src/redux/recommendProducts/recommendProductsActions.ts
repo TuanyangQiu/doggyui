eb49735926a4f7efa83a8fa6fe950b0d6e587c5c
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-30 19:56:38 +1200

    use id as the url query parameter

--- src/redux/recommendProducts/recommendProductsActions.ts
+++ src/redux/recommendProducts/recommendProductsActions.ts
@@ -55,7 +55,7 @@ export const FetchRecommendProductsActionCreator =
 
                 const tempProd = (resp.data as any[]).map((m, index) => {
                     return {
-                        id: index,
+                        id: m.Id,
                         title: m.Title,
                         touristRoutePictures: [{ url: "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg" }],
                         price: m.Price
