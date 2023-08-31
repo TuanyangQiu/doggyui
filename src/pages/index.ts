12b8551adeed3236f24724b371b536d78e33aa43
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-31 18:25:58 +1200

    create the ShoppingCartPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export * from './signIn';
 export * from './register';
 export * from './detail';
 export * from './searchResultsPage';
+export * from './shoppingCart';
---
+++ src/pages/shoppingCart/ShoppingCartPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from "./ShoppingCart.module.css";
+import { MainLayout } from "../../layouts"
+export const ShoppingCartPage: React.FC = () => {
+
+    return (
+        <MainLayout>
+            <h1>This is shopping car page</h1>
+        </MainLayout>
+    );
+}
