8e2f359bacc26537f7937f2d5f1ea64e478b4c0a
TuanyangQiu <qiutuanyang@gmail.com>
2023-09-02 11:16:10 +1200

    export new added slices

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,13 +9,17 @@ import { productSearchResultSlice } from "./productSearchResult/slice";
 import { UserSignInSlice } from "./userSignIn/slice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { shoppingCartSlice } from "./shoppingCart/slice";
+import { ordersSlice } from "./order/slice";
 //after used redux-toolkit, the combineReducer should come from redux toolkit instead of redux
 const rootReducer = combineReducers({
     language: languageReducer,
     recommendProducts: recommendProductsReducer,
     productDetailReducer: productDetailSlice.reducer,
     productSearchResultReducer: productSearchResultSlice.reducer,
-    userSignInReducer: UserSignInSlice.reducer
+    userSignInReducer: UserSignInSlice.reducer,
+    shoppingCartReducer: shoppingCartSlice.reducer,
+    orderReducer: ordersSlice.reducer
 });
 
 const persistConfig = {
@@ -30,7 +34,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
     // reducer: rootReducer,   use persistReducer to achieve data persist
     reducer: persistedReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(actionLog),
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: false,//to avoid the error "A non-serializable value was detected in an action"
+    }).concat(actionLog),
     devTools: true
 });
 const pstStore = persistStore(store);
