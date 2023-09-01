882943618684ee9c4233ffc8d2090e01156f51d0
TuanyangQiu <qiutuanyang@gmail.com>
2023-09-02 11:04:43 +1200

    add the button allowing user to add the product to shopping cart

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import axios from "axios";
-import { Spin, Row, Col, DatePicker, Divider, Typography, Anchor, Menu, MenuProps } from "antd";
-import { Header, Footer, ProductIntro, ProductComment } from "../../components";
+import { Spin, Row, Col, DatePicker, Divider, Typography, Anchor, Menu, MenuProps, Button } from "antd";
+import { ProductIntro, ProductComment } from "../../components";
 import Styles from './DetailPage.module.css';
 import { mockupComments, productPictures } from './mockupDetails';
-import { productDetailSlice, getProductDetailAsync } from '../../redux/productDetail/slice';
+import { getProductDetailAsync } from '../../redux/productDetail/slice';
 import { useSelector, useAppDispatch } from '../../redux/hooks';
-import { useDispatch } from 'react-redux';
 import { MainLayout } from "../../layouts";
-
+import { useTranslation } from "react-i18next";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { addShoppingCartItemAsync } from "../../redux/shoppingCart/slice";
 type MatchParams = {
     touristRouteId: string;
 }
@@ -38,13 +38,12 @@ const menuItems: MenuProps['items'] = [
 export const DetailPage: React.FC = () => {
 
     const { touristRouteId } = useParams<MatchParams>();
-    // const [loading, setLoading] = useState<boolean>(true);
-    // const [productInfo, setProductInfo] = useState<any>(null);
-    // const [requestError, setRequestError] = useState<string | null>(null);
     const loading = useSelector((state) => state.productDetailReducer.loading);
     const productInfo = useSelector(state => state.productDetailReducer.data);
     const requestError = useSelector(state => state.productDetailReducer.requestError);
-
+    const shoppingCartLoading = useSelector(state => state.shoppingCartReducer.loading);
+    const jwtToken = useSelector(state => state.userSignInReducer.jwtToken);
+    const { t } = useTranslation();
     const dispatch = useAppDispatch();// useDispatch();
 
     useEffect(() => {
@@ -74,62 +73,77 @@ export const DetailPage: React.FC = () => {
 
     return (
         <MainLayout>
-                {/* Product introduction and date selection */}
-                <div className={Styles["product-intro-container"]}>
-                    <Row>
-                        <Col span={13}>
-                            <ProductIntro
-                                title={productInfo.Title}
-                                shortDescription={productInfo.Description}
-                                price={productInfo.Price}
-                                coupons={"no cupon"}
-                                points={"no point"}
-                                discount={"no discount"}
-                                rating={productInfo.Rating}
-                                pictures={productPictures} />
-                            {/* pictures={productInfo.TouristRoutePictures.map(p => p.url)} /> */}
-                        </Col>
-                        <Col span={11}>
-                            <RangePicker open style={{ marginTop: 20 }} />
-                        </Col>
-                    </Row></div>
-                {/* Anchor Menu */}
-                <Anchor className={Styles["product-detail-anchor"]}>
-                    <Menu mode="horizontal" items={menuItems} />
-                </Anchor>
+            {/* Product introduction and date selection */}
+            <div className={Styles["product-intro-container"]}>
+                <Row>
+                    <Col span={13}>
+                        <ProductIntro
+                            title={productInfo.Title}
+                            shortDescription={productInfo.Description}
+                            price={productInfo.Price}
+                            coupons={"no cupon"}
+                            points={"no point"}
+                            discount={"no discount"}
+                            rating={productInfo.Rating}
+                            pictures={productPictures} />
+                        {/* pictures={productInfo.TouristRoutePictures.map(p => p.url)} /> */}
+                    </Col>
+                    <Col span={11} >
+                        <Button
+                            style={{ marginTop: 50, marginBottom: 30, display: "block" }}
+                            type="primary"
+                            danger
+                            loading={shoppingCartLoading}
+                            onClick={() => {
+                                dispatch(addShoppingCartItemAsync({
+                                    jwtToken: jwtToken as string,
+                                    productId: touristRouteId as string
+                                }))
+                            }}
+                        >
+                            <ShoppingCartOutlined />
+                            {t("productDetail.addToCart")}
+                        </Button>
+                        <RangePicker open style={{ marginTop: 20 }} />
+                    </Col>
+                </Row></div>
+            {/* Anchor Menu */}
+            <Anchor className={Styles["product-detail-anchor"]}>
+                <Menu mode="horizontal" items={menuItems} />
+            </Anchor>
 
-                {/* Product features */}
-                <div className={Styles["product-detail-container"]} id="feature">
-                    <Divider orientation="center">
-                        <Typography.Title level={3} >Features</Typography.Title>
-                    </Divider>
-                    <Typography.Text className={Styles["product-detail-content"]} >{productInfo.Features}</Typography.Text>
-                </div>
+            {/* Product features */}
+            <div className={Styles["product-detail-container"]} id="feature">
+                <Divider orientation="center">
+                    <Typography.Title level={3} >Features</Typography.Title>
+                </Divider>
+                <Typography.Text className={Styles["product-detail-content"]} >{productInfo.Features}</Typography.Text>
+            </div>
 
-                {/* Fees instrument */}
-                <div className={Styles["product-detail-container"]} id="fees">
-                    <Divider orientation="center">
-                        <Typography.Title level={3} >Fees</Typography.Title>
-                    </Divider>
-                    <Typography.Text className={Styles["product-detail-content"]}>{productInfo.Fees}</Typography.Text>
-                </div>
+            {/* Fees instrument */}
+            <div className={Styles["product-detail-container"]} id="fees">
+                <Divider orientation="center">
+                    <Typography.Title level={3} >Fees</Typography.Title>
+                </Divider>
+                <Typography.Text className={Styles["product-detail-content"]}>{productInfo.Fees}</Typography.Text>
+            </div>
 
 
-                {/* notes about the product */}
-                <div className={Styles["product-detail-container"]} id="notes">
-                    <Divider orientation="center">
-                        <Typography.Title level={3} >Notes</Typography.Title>
-                    </Divider>
-                    <Typography.Text className={Styles["product-detail-content"]} >{productInfo.Notes}</Typography.Text>
-                </div>
+            {/* notes about the product */}
+            <div className={Styles["product-detail-container"]} id="notes">
+                <Divider orientation="center">
+                    <Typography.Title level={3} >Notes</Typography.Title>
+                </Divider>
+                <Typography.Text className={Styles["product-detail-content"]} >{productInfo.Notes}</Typography.Text>
+            </div>
 
-                {/* comments */}
-                <div className={Styles["product-detail-container"]} id="comments">
-                    <Divider orientation="center">
-                        <Typography.Title level={3}>Comments</Typography.Title>
-                    </Divider>
-                    <ProductComment data={mockupComments} />
-                </div>
+            {/* comments */}
+            <div className={Styles["product-detail-container"]} id="comments">
+                <Divider orientation="center">
+                    <Typography.Title level={3}>Comments</Typography.Title>
+                </Divider>
+                <ProductComment data={mockupComments} />
+            </div>
         </MainLayout>
     )
 }
