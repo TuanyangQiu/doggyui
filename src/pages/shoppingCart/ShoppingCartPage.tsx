6e96ac46658e88ca651602832719e5abcb07e0db
TuanyangQiu <qiutuanyang@gmail.com>
2023-09-02 11:06:02 +1200

    display all shopping cart items and price

--- src/pages/shoppingCart/ShoppingCartPage.tsx
+++ src/pages/shoppingCart/ShoppingCartPage.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-import Styles from "./ShoppingCart.module.css";
-import { MainLayout } from "../../layouts"
+import Styles from "./ShoppingCartPage.module.css";
+import { MainLayout } from "../../layouts";
+import { Row, Col, Affix } from "antd";
+import { ProductList, PaymentCard, ShoppingCartItems } from "../../components";
+import { useSelector, useAppDispatch } from "../../redux/hooks";
+import { shoppingCartSlice, checkoutShoppingCartItemAsync } from "../../redux/shoppingCart/slice";
+import { useNavigate } from "react-router-dom";
 export const ShoppingCartPage: React.FC = () => {
-
+    const shoppingCartLoading = useSelector(state => state.shoppingCartReducer.loading);
+    const shoppingCartItems = useSelector(state => state.shoppingCartReducer.items);
+    const jwtToken = useSelector(state => state.userSignInReducer.jwtToken);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    // console.log("shoppingCartItems", jwtToken);
     return (
         <MainLayout>
-            <h1>This is shopping car page</h1>
+            <Row>
+                {/* All products in the shopping cart */}
+                <Col span={16} >
+                    <div className={Styles["product-list-container"]}>
+                        <ShoppingCartItems data={shoppingCartItems}  ></ShoppingCartItems>
+                    </div>
+                </Col>
+
+                {/* Total amount and payment */}
+                <Col span={8} >
+                    <Affix>
+                        <div className={Styles["payment-card-container"]}>
+                            <PaymentCard
+                                loading={shoppingCartLoading}
+                                originalPrice={Array.isArray(shoppingCartItems) ? shoppingCartItems
+                                    .map((s) => s.originalPrice)
+                                    .reduce((a, b) => a + b, 0) : 0}
+                                price={Array.isArray(shoppingCartItems) ? shoppingCartItems
+                                    .map((s) =>
+                                        s.originalPrice *
+                                        (s.discountPercent ? s.discountPercent : 1))
+                                    .reduce((a, b) => a + b, 0) : 0}
+                                onShoppingCartClear={() => { }}
+                                onCheckout={() => {
+                                    if (Array.isArray(shoppingCartItems) && shoppingCartItems.length > 0) {
+                                        dispatch(checkoutShoppingCartItemAsync(jwtToken as string));
+                                        navigate("/placeOrder");
+                                    }
+
+                                }} />
+                        </div>
+                    </Affix>
+                </Col>
+            </Row>
         </MainLayout>
     );
 }
